48b1fabb47ed9601b0a6ae664abd6df3b88a725c
ajeet2012 <ajeetmca2012@gmail.com>
2026-04-11 15:03:39 +0530

    Setting the iOS Project

---
+++ Pinch/Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Ajeet Sharma on 11/04/26.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
