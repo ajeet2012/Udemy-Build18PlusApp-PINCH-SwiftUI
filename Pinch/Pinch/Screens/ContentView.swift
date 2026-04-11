4719f50bf09713f2465ec07359facb6e46a7d84a
ajeet2012 <ajeetmca2012@gmail.com>
2026-04-11 16:35:08 +0530

    SwiftUI Drag Gesture

--- Pinch/Pinch/Screens/ContentView.swift
+++ Pinch/Pinch/Screens/ContentView.swift
@@ -13,8 +13,16 @@ struct ContentView: View {
     
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = .zero
     
     //MARK: - Function
+    
+    func resetImageState() {
+        return withAnimation(.spring()) {
+            imageScale = 1
+            imageOffset = .zero
+        }
+    }
     
     //MARK: - Content
     
@@ -33,6 +41,7 @@ struct ContentView: View {
                     .padding()
                     .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
                     .opacity(isAnimating ? 1 : 0)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
                     .scaleEffect(imageScale)
                 //MARK: - 1. TAP GESTURE
                     .onTapGesture(count: 2) {
@@ -42,11 +51,25 @@ struct ContentView: View {
                             }
                         }
                         else {
-                            withAnimation(.spring()) {
-                                imageScale = 1
-                            }
+                            resetImageState()
                         }
                     }
+                //MARK: - 1. DRAGE GESTURE
+                    .gesture(
+                        DragGesture()
+                            .onChanged { value in
+                                print(value.translation)
+                                withAnimation(.linear(duration: 1)){
+                                    imageOffset = value.translation
+                                }
+                            }
+                            .onEnded { _ in
+                                if imageScale <= 1 {
+                                    resetImageState()
+                                }
+                                
+                            }
+                    )
                 
                 
                 
